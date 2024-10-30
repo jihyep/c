0c6163e204b03bfa9caa2fdd7bfc5cf25ee65bc1
jihyep <jihyepark@sookmyung.ac.kr>
2024-10-30 14:27:53 +0900

    week09

---
+++ week09/1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int flag =1;
+	while(flag!=0)
+	{
+		int y;
+		y=3;
+		flag=0;
+	}
+	
+}
---
+++ week09/5-1main.c
@@ -0,0 +1,24 @@
+/* #include <stdio.h>
+int all_files;
+
+extern void sub();
+
+int main(void)
+{
+	sub();
+	printf("%d\n", all_files);
+	return 0;
+} */
+
+#include <stdio.h>
+static int all_files;
+
+extern void sub();
+
+int main(void)
+{
+	sub();
+	printf("%d\n", all_files);
+	return 0;
+}
+
