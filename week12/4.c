e3b370758cf7f7eea9bacc9427c0b29c0704b87f
jihyep <jihyepark@sookmyung.ac.kr>
2024-11-20 14:47:00 +0900

    week12

---
+++ week12/4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int grade[5];
+	
+	
+	for (i=0; i<5; i++)
+	{
+		printf("Input value - grade[%i] =", i);
+		scanf("%d", &grade[i]);
+	}
+	
+	int *ptr;
+	ptr=grade;
+	int sum=0;
+	for (i=0; i<5; i++)
+	{
+		printf("grade[%i] = %d\n", i, *(ptr+i));
+		sum+=*(ptr+i);
+	}
+	
+	printf("average : %d\n", sum/5);
+	
+	
+	
+	return 0;
+}
